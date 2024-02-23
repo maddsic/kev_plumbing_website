8334760c490d019a1cbe04babfa3aa4190cfc147
maddsic <sainsidibeh@yahoo.com>
2024-02-23 03:51:09 +0000

    refactor 3

--- components/Testimonial/index.tsx
+++ components/Testimonial/index.tsx
@@ -34,7 +34,7 @@ const Testimonials = () => {
                {testimonialData.map((tes, tesIndex) => (
                   <Card
                      key={tesIndex}
-                     className="w-full mb-3 shadow-lg hover:shadow-md rounded-2xl sm:p-8 "
+                     className="w-full mb-3 shadow-lg hover:shadow-md rounded-2xl sm:p-8 overflow-hidden"
                   >
                      <div className="flex justify-end p-1">
                         <Quote color="gray" fill="gray" />
