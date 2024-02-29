76acd378fdbd147bfe5f13e2a251622ca428102d
maddsic <sainsidibeh@yahoo.com>
2024-02-29 21:50:15 +0000

    improved styling

--- app/robots.ts
+++ app/robots.ts
@@ -3,8 +3,7 @@ import { MetadataRoute } from "next";
 export default function robot(): MetadataRoute.Robots {
    const baseUrl =
       process.env.NEXT_PUBLIC_BASE_URL?.replace(/\/$/, "") ||
-      process.env.NEXT_PUBLIC_BASE_URL; // Remove trailing slash if exists
-
+      "https://dunedinplumbing.co.uk";
    return {
       rules: [
          {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
    return [
       {
-         url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+         url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
          lastModified: new Date(),
          changeFrequency: "weekly",
          priority: 1,
