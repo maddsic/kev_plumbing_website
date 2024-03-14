e570ac388e818394bfe90b489246984d8e6abd13
maddsic <sainsidibeh@yahoo.com>
2024-03-14 00:53:24 +0000

    token

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -6,7 +6,7 @@ export const client = createClient({
    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
    apiVersion: process.env.NEXT_PUBLIC_SANITY_APIVERSION,
    useCdn: false,
-   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
